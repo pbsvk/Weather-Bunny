ccd9b7286cc70de8898e6dcffdffef2d5377397b
Vamsi <bpadala@stevens.edu>
2024-11-23 18:46:17 -0500

    Initial Commit

---
+++ Weather Bunny/Weather_BunnyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_BunnyApp.swift
+//  Weather Bunny
+//
+//  Created by Bhaskara Padala on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_BunnyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weather BunnyTests/Weather_BunnyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_BunnyTests.swift
+//  Weather BunnyTests
+//
+//  Created by Bhaskara Padala on 11/23/24.
+//
+
+import Testing
+@testable import Weather_Bunny
+
+struct Weather_BunnyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
