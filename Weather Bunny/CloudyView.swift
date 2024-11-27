c8001eafb04c6ca37be4ea46bc837f3c2136e0e0
Vamsi <bpadala@stevens.edu>
2024-11-27 11:18:41 -0500

    SunnyView, NightView - prototypes were being made

---
+++ Weather Bunny/CloudyView.swift
@@ -0,0 +1,18 @@
+//
+//  CloudyView.swift
+//  Weather Bunny
+//
+//  Created by Bhaskara Padala on 11/26/24.
+//
+
+import SwiftUI
+
+struct CloudyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CloudyView()
+}
---
+++ Weather Bunny/RainyView.swift
@@ -0,0 +1,18 @@
+//
+//  RainyView.swift
+//  Weather Bunny
+//
+//  Created by Bhaskara Padala on 11/26/24.
+//
+
+import SwiftUI
+
+struct RainyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RainyView()
+}
